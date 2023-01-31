bae275be988597a08eaf8f91919238814e141e83
guozijian <sole0219@163.com>
2023-01-31 17:51:36 +0800

    feat:路由跳转token验证

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,47 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/Home.vue";
+import { getCookie } from '@/utils/util.cookie'
+import Nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    redirect: '/login'
   },
-  // {
-  //   path: "/about",
-  //   name: "about",
-  //   component: () =>
-  //     import( "../views/AboutView.vue"),
-  // },
+  {
+    path: "/login",
+    name: "login",
+    component: import("@/views/Home.vue"),
+  },
+  {
+    path: "/userHome",
+    name: "userHome",
+    component: () =>
+      import("@/views/user/userHome.vue"),
+  },
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
 });
+
+router.beforeEach((to, from, next) => {
+  // 进度条
+  Nprogress.start()
+  if (to.name !== 'login') {
+    let token = getCookie('token')
+    if (token) {
+      next()
+      Nprogress.done()
+    } else {
+      next('/login')
+      Nprogress.done()
+    }
+  } else {
+    next()
+    Nprogress.done()
+  }
+})
 
 export default router;
