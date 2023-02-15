c40207e9a2ddb6f0bb69c83187a1e38116410103
jiangjingran <ran990219@163.com>
2023-02-15 17:05:19 +0800

    feat：个人中心初版

--- src/views/partUser/personalConter.vue
+++ src/views/partUser/personalConter.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="personal-conter">
     <div class="personal-header">
-      <div class="trunBack">返 回</div>
+      <div class="trunBack" @click="turnBack">返 回</div>
     </div>
     <div class="personal-body">
       <el-card class="box-card">
@@ -9,12 +9,54 @@
         <div class="card-content">{{ store.userInfo.nickname }}</div>
       </el-card>
       <el-card class="box-BottomCard">
-        <template #header>
-          <div class="card-header">
-            <span>个人资料</span>
-          </div>
-          <!-- <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div> -->
-        </template>
+        <div class="card-header">
+          <span>个人资料</span>
+        </div>
+        <div v-for="o in sidebarItem" :key="o" class="text-item">{{ o }}</div>
+        <div class="card-header">
+          <span>消息中心</span>
+        </div>
+        <div v-for="o in sidebarXItem" :key="o" class="text-item">{{ o }}</div>
+        <div class="card-header">
+          <span>XXXX</span>
+        </div>
+        <div v-for="o in sidebarXxItem" :key="o" class="text-item">{{ o }}</div>
+      </el-card>
+      <el-card class="box-RightTopCard">
+        <span>个人资料</span>
+      </el-card>
+      <el-card class="box-RightCard">
+        <el-tabs v-model="activeName" class="demo-tabs">
+          <el-tab-pane label="基本资料" name="first">
+            <el-form :model="userForm" label-width="140px" disabled class="formStyle">
+              <el-form-item label="用户名:" prop="username">
+                <el-input v-model="userForm.username" />
+              </el-form-item>
+              <el-form-item label="昵称:" prop="nickname">
+                <el-input v-model="userForm.nickname" />
+              </el-form-item>
+              <el-form-item label="邮箱:" prop="email">
+                <el-input v-model="userForm.email" />
+              </el-form-item>
+              <el-form-item label="权限等级:" prop="power">
+                <el-select v-model="userForm.power" class="m-2" placeholder="选择权限等级">
+                  <el-option
+                    v-for="item in levelDict"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  />
+                </el-select>
+              </el-form-item>
+              <el-form-item label="头像:">
+                <img v-if="userForm.imgUrl" :src="userForm.imgUrl" class="avatar" />
+              </el-form-item>
+            </el-form>
+          </el-tab-pane>
+          <el-tab-pane label="修改头像" name="second">修改头像</el-tab-pane>
+          <el-tab-pane label="修改密码" name="third">修改密码</el-tab-pane>
+          <el-tab-pane label="身份认证" name="fourth">身份认证</el-tab-pane>
+        </el-tabs>
       </el-card>
     </div>
   </div>
@@ -22,14 +64,38 @@
 
 <script lang="ts" setup>
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import type { TabsPaneContext } from 'element-plus'
 import { loginStore } from '@/storePinia/index'
+import { reactive } from 'vue'
 
+const activeName = ref('first')
+// 表单数据
+let userForm = reactive({
+  username: '',
+  nickname: '',
+  email: '',
+  power: 'oridinary',
+  status: 1,
+  imgUrl: ''
+})
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+  console.log(tab, event)
+}
 const fileImg = ref('')
+const router = useRouter()
 const store = loginStore()
 const demoImg = require('@/assets/imgs/ava.png')
-
+const sidebarItem: Array<string> = ['基本资料', '修改头像', '修改密码', '身份认证']
+const sidebarXItem: Array<string> = ['系统通知', '私信', '资金管理']
+const sidebarXxItem: Array<string> = ['xxx', 'xx', 'xxxx', 'xxx']
+userForm = store.userInfo
 // 判断该账户是否有头像，如有则使用，无则用默认
 fileImg.value = store.userInfo.imgUrl
+
+const turnBack = () => {
+  router.push('/userHome')
+}
 </script>
 <style lang="scss" scoped>
 .personal-header {
@@ -52,9 +118,9 @@ fileImg.value = store.userInfo.imgUrl
   }
 }
 .personal-body {
+  position: relative;
   width: 75%;
   height: 800px;
-  background-color: pink;
   margin: 30px auto;
   .box-card {
     text-align: center;
@@ -70,10 +136,51 @@ fileImg.value = store.userInfo.imgUrl
     .card-header {
       text-align: center;
       font-size: 22px;
+      padding: 5px;
+      border-bottom: 1px solid #ccc;
+    }
+    .text-item {
+      margin: 10px 106px;
+      font-size: 16px;
+      cursor: pointer;
+    }
+  }
+  .box-RightTopCard {
+    position: absolute;
+    width: 72%;
+    height: 55px;
+    top: 0;
+    right: 0;
+    line-height: 25px;
+    font-size: 22px;
+  }
+  .box-RightCard {
+    position: absolute;
+    width: 72%;
+    height: 89%;
+    top: 62px;
+    right: 0;
+    .formStyle {
+      padding-top: 60px;
+      width: 600px;
+      .avatar {
+        width: 178px;
+        height: 178px;
+        display: block;
+      }
+    }
+    .demo-tabs > .el-tabs__content {
+      padding: 32px;
+      color: #6b778c;
+      font-size: 32px;
+      font-weight: 600;
     }
   }
 }
 ::v-deep .el-card__header {
   padding: 10px 0;
+}
+::v-deep .el-card__body {
+  padding: 15px;
 }
 </style>
